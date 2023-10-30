8d257d1b5c6c2a55b413a0d26becb9b551e842dc
Fekadu Tadesse <aaumsccs@gmail.com>
2023-10-30 12:27:47 +0300

    update the database string

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 const express = require("express");
-// const mongoose = require("mongoose");
 const cors = require("cors");
 const TodoModel = require("./Models/Todo");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-
-// const dbUrl = "mongodb://127.0.0.1:27017/TodoList";
-
-// mongoose.connect(dbUrl);
 
 require("dotenv").config();
 
